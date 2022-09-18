9f9f7d7968a047d6196872075343ada2638d776a
Colourful0 <1747529563@qq.com>
2022-09-18 12:11:17 +0800

    add some code from Mac

--- autoSyn.sh
+++ autoSyn.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+git init 
 git status  
 git add .  
 git commit -m 'add some code from Mac'
 # git commit -m 'add some results from Server'
+git remote add origin https://github.com/Colourful10/MyRepositories.git
 git pull --rebase origin master   #domnload data
 git push origin master            #upload data
 git stash pop

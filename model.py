b6e8c8db44ca83e59f0900f3554f5c83f733a74b
Colourful0 <1747529563@qq.com>
2022-09-18 12:00:07 +0800

    add some code from Mac

---
+++ model.py
@@ -0,0 +1,12 @@
+import timm
+import torch
+# from fastai.version.all import *
+
+avail_pretrained_models = timm.list_models(pretrained=True)
+
+model = timm.create_model('resnet34', pretrained=True)
+
+print(model)
+
+# torch.randn(1,1,32,32)是batch_size=1, 图像通道数为1,图像尺寸为32x32
+print(model.default_cfg)
